d0cefc3b0534fd7e52ad1309eda4b4c74e2b4f76
jwc20 <choi.jae100@gmail.com>
2025-07-27 15:23:28 +0900

    add dynamic opacity of line

--- particle-systems/script.js
+++ particle-systems/script.js
@@ -3,19 +3,18 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
 const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
 gradient.addColorStop(0, "white");
 gradient.addColorStop(0.5, "magenta");
 gradient.addColorStop(1, "blue");
-
 ctx.fillStyle = gradient;
+ctx.strokeStyle = "white";
 
 class Particle {
   // for indiv particles
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 10 + 5;
+    this.radius = Math.random() * 5 + 2;
     this.x =
       this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y =
@@ -24,11 +23,10 @@ class Particle {
     this.vy = Math.random() * 4 - 2;
   }
   draw(context) {
-    // context.fillStyle = "hsl(" + this.x * 0.5 + ", 100%, 50%)";
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
-    context.stroke();
+    // context.stroke();
   }
   update() {
     this.x += this.vx;
@@ -58,11 +56,11 @@ class Effect {
     }
   }
   handleParticles(context) {
+    this.connectParticles(context);
     this.particles.forEach((particle) => {
       particle.draw(context);
       particle.update();
     });
-    this.connectParticles(context);
   }
   connectParticles(context) {
     const maxDistance = 100;
@@ -70,23 +68,24 @@ class Effect {
       for (let b = a; b < this.particles.length; b++) {
         const dx = this.particles[a].x - this.particles[b].x;
         const dy = this.particles[a].y - this.particles[b].y;
-        const distance = Math.hypot(dx,dy);
+        const distance = Math.hypot(dx, dy);
 
         if (distance < maxDistance) {
-          context.strokeStyle = "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+          context.save();
+          const opacity = 1 - distance / maxDistance;
+          context.globalAlpha = opacity;
           context.beginPath();
           context.moveTo(this.particles[a].x, this.particles[a].y);
           context.lineTo(this.particles[b].x, this.particles[b].y);
           context.stroke();
+          context.restore();
         }
-
       }
     }
   }
 }
 
 const effect = new Effect(canvas);
-
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
