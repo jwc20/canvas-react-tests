17b8ff41a0199d34082ccf2dd399bc0e425d147f
jwc20 <choi.jae100@gmail.com>
2025-07-27 16:04:26 +0900

    add particle physics

--- particle-systems/script.js
+++ particle-systems/script.js
@@ -14,19 +14,21 @@ class Particle {
   // for indiv particles
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 5 + 2;
+    this.radius = Math.floor(Math.random() * 10 + 1);
     this.x =
       this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y =
       this.radius + Math.random() * (this.effect.height - this.radius * 2);
-    this.vx = Math.random() * 4 - 2;
-    this.vy = Math.random() * 4 - 2;
+    this.vx = Math.random() * 1 - 0.5;
+    this.vy = Math.random() * 1 - 0.5;
+    this.pushX = 0;
+    this.pushY = 0;
+    this.friction = 0.95;
   }
   draw(context) {
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
-    // context.stroke();
   }
   update() {
     if (this.effect.mouse.pressed) {
@@ -36,10 +38,13 @@ class Particle {
       const force = this.effect.mouse.radius / distance;
       if (distance < this.effect.mouse.radius) {
         const angle = Math.atan2(dy, dx);
-        this.x += Math.cos(angle) * force;
-        this.y += Math.sin(angle) * force;
+        this.pushX += Math.cos(angle) * force;
+        this.pushY += Math.sin(angle) * force;
       }
     }
+
+    this.x += (this.pushX *= this.friction) + this.vx;
+    this.y += (this.pushY *= this.friction) + this.vy;
 
     if (this.x < this.radius) {
       this.x = this.radius;
@@ -55,18 +60,6 @@ class Particle {
       this.y = this.effect.height - this.radius;
       this.vy *= -1;
     }
-    this.x += this.vx;
-    this.y += this.vy;
-
-    // this.x += this.vx;
-    // if (this.x > this.effect.width - this.radius || this.x < this.radius) {
-    //   this.vx *= -1;
-    // }
-
-    // this.y += this.vy;
-    // if (this.y > this.effect.height - this.radius || this.y < this.radius) {
-    //   this.vy *= -1;
-    // }
   }
   reset() {
     this.x =
