e874dd08ed4cebdfe4151b88cf7302e90d85ebd0
jwc20 <choi.jae100@gmail.com>
2025-07-27 14:35:02 +0900

    add copy

---
+++ particle-systems-bounce/script.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.fillStyle = "white";
+
+class Particle {
+  // for indiv particles
+  constructor(effect) {
+    this.effect = effect;
+    this.radius = 15;
+    this.x =
+      this.radius + Math.random() * (this.effect.width - this.radius * 2);
+    this.y =
+      this.radius + Math.random() * (this.effect.height - this.radius * 2);
+    this.vx = Math.random() * 4 - 2;
+    this.vy = Math.random() * 4 - 2;
+  }
+  draw(context) {
+    context.fillStyle = "hsl(" + this.x * 0.5 + ", 100%, 50%)";
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+  }
+  update() {
+    this.x += this.vx;
+    if (this.x > this.effect.width - this.radius || this.x < this.radius) {
+      this.vx *= -1;
+    }
+
+    this.y += this.vy;
+    if (this.y > this.effect.height - this.radius || this.y < this.radius) {
+      this.vy *= -1;
+    }
+  }
+}
+
+class Effect {
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.particles = [];
+    this.numberOfParticles = 200;
+    this.createParticles();
+  }
+  createParticles() {
+    for (let i = 0; i < this.numberOfParticles; i++) {
+      this.particles.push(new Particle(this));
+    }
+  }
+  handleParticles(context) {
+    this.particles.forEach((particle) => {
+      particle.draw(context);
+      particle.update();
+    });
+  }
+}
+
+const effect = new Effect(canvas);
+
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  effect.handleParticles(ctx);
+  requestAnimationFrame(animate);
+}
+animate();
