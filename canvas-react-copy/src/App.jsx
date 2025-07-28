09890d316e16c8344e5f5fba5a222e552edc6b4b
jwc20 <choi.jae100@gmail.com>
2025-07-28 10:58:59 +0900

    add copy with grid system

---
+++ canvas-react-copy/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ParticleCanvas from "./components/ParticleCanvas.jsx";
+import GridCanvas from "./components/GridCanvas.jsx";
+
+function App() {
+  return (
+    <>
+
+      <GridCanvas />
+      <ParticleCanvas />
+    </>
+  );
+}
+
+export default App;
